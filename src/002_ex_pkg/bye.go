ee158b4412cfd76c4fee8d7ad059b26745dca859
AbhishekJ <abhishekjaiswal.kol@gmail.com>
2019-09-03 18:55:05 +0530

    reset

--- src/002_ex_pkg/bye.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-// package scope
-func bye() {
-	// block scope
-	fmt.Println("Bye Gopher ! from bye.go file")
-}
--- src/002_ex_pkg/hey.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-// package scope
-func hey() {
-	//block scope
-	fmt.Println("Hey Gopher ! from hey.go file")
-}
